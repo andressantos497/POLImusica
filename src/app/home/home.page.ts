f78de341e14a6dc4e389addd959e771454f4f250
Your Name <andressantos497@gmail.com>
2024-07-11 20:33:03 -0500

    slides dinamicos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+slides = [
+  {
+    tittle: "Titulo de slide 1",
+    icon: "beer-outline",
+    avatar: "https://i.pinimg.com/736x/9b/0d/a2/9b0da29b8cf529c80f503064699b255d.jpg",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRpMHrmXCI7fo5ooA909SCkPeYe3WFo7Yaxw&s",
+    description: "Este es el avatar real"
+  },
+  {
+    tittle: "Titulo de slide 2",
+    icon: "beer-outline",
+    avatar: "https://i.pinimg.com/736x/9b/0d/a2/9b0da29b8cf529c80f503064699b255d.jpg",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRpMHrmXCI7fo5ooA909SCkPeYe3WFo7Yaxw&s",
+    description: "Este es el avatar real"
+  },
+  {
+    tittle: "Titulo de slide 3",
+    icon: "beer-outline",
+    avatar: "https://i.pinimg.com/736x/9b/0d/a2/9b0da29b8cf529c80f503064699b255d.jpg",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRpMHrmXCI7fo5ooA909SCkPeYe3WFo7Yaxw&s",
+    description: "Este es el avatar real"
+  }
+  
+]
+
   constructor() {}
 
 }
