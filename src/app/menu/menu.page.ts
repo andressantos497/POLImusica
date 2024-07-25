4ce2ed5d8ab3f5bd51d4841c29bd0297b6fb2e2b
Your Name <andressantos497@gmail.com>
2024-07-24 20:36:37 -0500

    componentes menu

---
+++ src/app/menu/menu.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController, NavController } from '@ionic/angular';
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.page.html',
+  styleUrls: ['./menu.page.scss'],
+})
+export class MenuPage implements OnInit {
+
+  constructor(private menu: MenuController, private navCtrl: NavController) { }
+
+  ngOnInit() {
+  }
+
+  closeMenu(){
+    console.log("cerrar menu");
+    this.menu.close();
+  }
+
+  logout(){
+    console.log("cerrar session");
+    this.navCtrl.navigateRoot("/login");
+  }
+
+}
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 import { Storage } from '@ionic/storage-angular';
+import { AuthenticateService } from '../services/authenticate.service';
 
 @Component({
   selector: 'app-register',
@@ -9,13 +10,32 @@ import { Storage } from '@ionic/storage-angular';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-registerForm: FormGroup;
+  registerForm: FormGroup;
+  validation_messages = {
+    name: [
+      { type: 'required', message: 'El nombre es obligatorio' },
+      { type: 'pattern', message: 'El nombre solo puede contener letras y espacios' }
+    ],
+    last_name: [
+      { type: 'required', message: 'El apellido es obligatorio' },
+      { type: 'pattern', message: 'El apellido solo puede contener letras y espacios' }
+    ],
+    email: [
+      { type: 'required', message: 'El email es obligatorio' },
+      { type: 'email', message: 'Email inválido' }
+    ],
+    password: [
+      { type: 'required', message: 'La contraseña es obligatoria' },
+      { type: 'minlength', message: 'La contraseña debe tener al menos 6 caracteres' }
+    ]
+  };
 
   constructor(
     private formBuilder: FormBuilder,
     private navCtrl: NavController,
-    private storage: Storage
-  ) { 
+    private storage: Storage,
+    private authService: AuthenticateService
+  ) {
     this.registerForm = this.formBuilder.group({
       email: new FormControl(
         "",
@@ -38,18 +58,27 @@ registerForm: FormGroup;
           Validators.pattern('^[a-zA-Z ]+$')
         ])
       ),
-      Last_name: new FormControl(
+      last_name: new FormControl(
         "",
         Validators.compose([
           Validators.required,
           Validators.pattern('^[a-zA-Z ]+$')
         ])
       )
-    })
+    });
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  goToLogin(){
+    this.navCtrl.navigateBack("/login")
+  }
+  
+  register(registerData: any){
+    console.log(registerData);
+    this.authService.registerUser(registerData).then(res => {
+      this.navCtrl.navigateBack("/login");
+    });
   }
 
- 
 }
